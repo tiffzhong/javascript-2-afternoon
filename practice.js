b06f594fd01d361e24d89caf9e970990ee438a86
Tiffany Zhong <tiffzhong@gmail.com>
2018-11-13 16:14:57 -0700

    completed assignment

--- practice.js
+++ practice.js
@@ -14,9 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
-
-
+var me = {
+  name: 'Tiffany',
+  age: 25,
+}
+console.log(me.name);
 
 ////////// PROBLEM 2 //////////
 
@@ -25,25 +27,27 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
-
-
+var favoriteThings = {
+  band: 'Good Charolette',
+  food: 'salad', 
+  person: 'CJ', 
+  book: 'Ready Player One', 
+  movie: 'Tarzan', 
+  holiday: 'Chinese New Year'
+}
 
 /*
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
-
-
+favoriteThings.car = 'Subaru';
+favoriteThings.brand = 'Uniqlo';
 
 /*
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
-
-//Code here
-
-
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 ////////// PROBLEM 3 //////////
 
@@ -55,16 +59,18 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+var backPack = {
 
+};
+var item = 'firstPocket';
+backPack['firstPocket'] = item;
+backPack['firstPocket'] = 'chapstick';
 
 
 /*
   After you do the above, alert your entire backPack object.
 */
-
-//Code here
-
+//alert(backPack)
 
 
 /*
@@ -72,10 +78,7 @@ You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
-
-//Code here
-
-
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -96,21 +99,11 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
-
-
-
-
-
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
-
-
-
-
-
 
 
 ////////// PROBLEM 5 //////////
